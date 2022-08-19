43d5135e4a79685e140e5179a05daa46207823e2
Sojin Lee <31687412+lsj90954511@users.noreply.github.com>
2022-08-19 15:56:11 +0900

    로딩화면 다음 씬 넘어가는 것 까지

---
+++ MedicineA/Assets/ChangeScene.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            SceneManager.LoadScene("Main");
+    }
+}
